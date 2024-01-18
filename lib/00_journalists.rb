6c9734f81fa2fa1357633583b61a3864c547a938
NicolasBaldo <septfoisept@outlook.fr>
2024-01-18 15:33:02 +0100

    fin du projet

---
+++ lib/00_journalists.rb
@@ -0,0 +1,57 @@
+handles = ["@jcunniet", "@PaulLampon", "@Aziliz31", "@ssoumier", "@marionsouzeau", "@gaellombart", "@bendarag", "@AurelieLebelle", "@julienduffe", "@thomaspoupeau", "@LilyRossignol", "@ClairGuedon", "@stephanieauguy", "@claw_prolongeau", "@_JulieMenard", "@LColcomb", "@Zlauwereys", "@MeLonguet", "@DorotheeLN", "@NolwennCosson", "@ADaboval", "@Remibaldy", "@bderveaux", "@amandechap", "@ELODIESOULIE", "@nbongarcon", "@HeloAb", "@Panamorama", "@gregplou", "@BenoitBerthe", "@LauraBruneau89", "@Anthony_Lieures", "@Sharonwaj", "@mcsonkin", "@pverduzier", "@emiliel3", "@Julien_MARION", "@SophiFay", "@bdelombre", "@annalecerf", "@AdriaBudry", "@DejNikolic", "@iJaffre", "@CyrusleVirus", "@GPardigon", "@e_vallerey", "@IsabelleSouquet", "@AudeDavidRossi", "@Yoann_Pa", "@CeliaPenavaire", "@perraultvincent", "@cboulate", "@JustineWeyl", "@Paulinejacot", "@juliechab", "@aslechevallier", "@phnou", "@Seb_Pommier", "@Alex_Bensaid", "@GuillaumeGaven", "@annelaurechouin", "@Oliviader", "@guerricp", "@JMMarchaut", "@cyceron", "@gregory_raymond", "@vhunsinger", "@l_peillon", "@fannyguinochet", "@EAssayag", "@KibweAdom", "@YvonHerry", "@JohanGarciajg", "@saidlabidi", "@lauranneprov", "@LeaDavy", "@francois_remy", "@CGuicheteau", "@FloMaillet", "@m_perroud", "@oBrunet_TSMF", "@MoonVdc", "@jc2taille", "@NellyMoussu", "@VirginK", "@b_misa", "@FabriceCouste", "@barbara_prose", "@lelia2m", "@brunoaskenazi", "@laurenechamp", "@ysisbox", "@juliengagliardi", "@PierreLel", "@kdeniau", "@_TerraInc", "@DominicArpin", "@antoinfonteneau", "@nanotousch", "@jb_roch", "@YaniKhezzar", "@Anne_Bechet", "@NCapart", "@SamyBenNaceur", "@Joumany", "@Julietteraynal", "@TGiraudet", "@SaraTanit", "@HappeFrederic", "@antoinellorca", "@michelpicot", "@Sev_Ryne", "@bobdobolino", "@murdever", "@YGrandmontagne", "@Mnyo", "@EdKOSCIANSKI", "@tnoisette", "@jankari", "@delbello_rom", "@rflechaux", "@NadiaSorelli", "@IT_Digital", "@abarbaux", "@PhilippeLeroy", "@schaptal", "@marionspee", "@lisavignoli", "@ChloeAeberhardt", "@MartineJacot", "@JuliaPascualita", "@curieusedetout", "@sgraveleau", "@bif_o", "@ElisaPineau", "@zinebdryef", "@apiquard", "@pierrehaski", "@StephanieDelmas", "@Blandine_Garot", "@vergara_i", "@evan_lebastard", "@SophieVclt", "@OlivierLevrault", "@alicedorgeval", "@LouiseMalnoy", "@alix_fx", "@pierre_baudis", "@LucMagoutier", "@AgatheMuller", "@SGianninelli", "@PaulineBoyer33", "@NaomiHalll", "@romaindlx", "@marionbr", "@Burtschy", "@JacobEtienne", "@as_lizzani", "@marie_simon", "@LaureDaussy", "@FabriceAmedeo", "@min", "@LoubnaChlaikhy", "@PlummerWilliam", "@OlivierMarin1", "@alaurefremont", "@mwesfreid", "@ChBaDe", "@pmathon", "@theobaldmarie", "@Lnpagesy", "@marclandre", "@paoliniesther", "@Feertchak", "@JBLitzler", "@GuillaumeErrard", "@quentinperinel", "@TristanQM", "@mlbo", "@constancejamet", "@LoraTerrazas", "@emiliegeffray", "@Mathilde_Sd", "@CaroPiquet", "@DCanivez", "@TIM_7375", "@blandinelc", "@ivanrioufol", "@arthurberdah", "@SarahLecoeuvre", "@guillaume_gui", "@DamienMercereau", "@W_Chloe", "@Assma_MD", "@EugenieBastie", "@HiTech_lexpress", "@bcondominas", "@Laurie_Z_", "@jeanfrancgerard", "@MathieuPagura", "@BGUYF", "@AlainPiffaretti", "@AudreyKucinskas", "@julienhory", "@Pierrefalga", "@TiphThuillier", "@cdaniez", "@LigerBaptiste", "@D_Peras", "@julie_dlb", "@Fatiha_Temmouri", "@julian2lamancha", "@GaetaneDeljurie", "@JulianMattei", "@M_Vicuna", "@DeBruynOlivier", "@Nehed_Jendoubi", "@antoine_grenapi", "@ColonnaGen", "@VictoriaGairin", "@Clement_Lacombe", "@TVigoureux", "@MargauxObriot", "@solinedelos", "@RocheSabine", "@dangerkens", "@EdouardDutour", "@MDondeyne", "@DupuisNathalie1", "@bouscarel", "@Mathieu2jean", "@Sophie_T_J", "@laurentcalixte", "@patrockwilliams", "@PascaleKremer", "@AlexJaquin", "@LauraIsaaz", "@cath_robin", "@Del_Gautherin", "@Isaduriez", "@lucietuile", "@AugeyBastien", "@mcastagnet", "@AminaKalache", "@mvaudano", "@CParrot", "@ombelinetips", "@_JoinLion", "@BarbolosiRose", "@ToiBruno1", "@FloraClodic", "@xjbdx", "@ceci_est_un_handle_vraiment_long", "@AlexiaEy", "@Emjy_STARK", "@elcoco01", "@rabilebon", "@pflovens_", "@FabriceFrossard", "@MorganeSERRES", "@MarjolaineKoch", "@edgarsnow", "@SRNLF", "@CChassigneux", "@KerinecMoran", "@NassiraELM", "@NewsRicard", "@Sandreene", "@Emilezrt", "@Pierre_Do", "@Micode", "@CColumelli", "@DavidAbiker", "@ClementBergantz", "@benjaminrabier", "@celinekallmann", "@edwyplenel", "@C_Barbier", "@JJBourdin_RMC", "@LaurenceFerrari", "@aslapix", "@IsaMillet", "@MaximeSwitek", "@tomjoubert", "@jszanchi", "@roqueeva", "@XavierBiseul", "@florencesantrot", "@AntoineCrochet", "@freeman59", "@MaudeML", "@philippe_gulpi", "@mathieum76", "@kiouari", "@imanemoustakir", "@BenedicteMallet", "@Emilie_Brouze", "@antoinebarret", "@_nicolasbocquet", "@remibuhagiar", "@CourretB", "@AymericRobert", "@miraelmartins", "@pmaniere", "@jesuisraphk", "@David_Ingram", "@pcelerier", "@technomedia", "@Geraldinedauver", "@ThierryLabro", "@Newsdusud", "@nrauline", "@gbregeras", "@SCouasnonBFM", "@actualites_nrv", "@dimitrimoulins", "@oli_aura", "@FabieChiche", "@Vincent_Raimblt", "@ChristophGreuet", "@PAlbuchay", "@MarrauddesGrot", "@vtalmon", "@cedric", "@oliviertesquet", "@Julien_Jay", "@LydiaBerroyer", "@Shuua", "@datisdaz", "@Steuph", "@ameliecharnay", "@Bruno_LesNums", "@LelloucheNico", "@CciliaDiQuinzio", "@Elodie_C", "@SylvRolland", "@Kocobe", "@FL_Debes", "@jdupontcalbo", "@GarciaVictor_", "@NicoRichaud", "@RHoueix", "@simottel", "@DamienLicata", "@annabelle_L", "@Lea_Lejeune", "@axel_deb", "@marin_eben", "@ptiberry", "@MatthieuDelach", "@sandrinecassini", "@benjaminferran", "@ppgarcia75", "@NotPatrick", "@ivalerio", "@FabienneSchmitt", "@alexgoude", "@JeromeColombain", "@manhack", "@Capucine_Cousin", "@Fsorel", "@oliviertesquet", "@marjoriepaillon", "@ginades", "@PierreTran", "@DelphineCuny", "@reesmarc", "@lauratenoudji", "@ldupin", "@carolinedescham", "@Lucile_Quillet", "@cgabizon", "@Allocab", "@epenser", "@JAGventeprivee", "@frwrds", "@Laure__Bourdon", "@Xavier75", "@maximeverner", "@s_jourdain", "@LoriHelloc"]
+
+# QUESTION 1: Combien y a-t-il de handle dans cette array ?
+puts "1. Il y a #{handles.length} handles dans cette array."
+#Utilisiation de la fonction .length afin d'obtenir la longueur de tableau de l'array "handles"
+
+
+# QUESTION 2: Quel est le handle le plus court de cette liste ?
+shortest_handle = handles.min_by { |handle| handle.length }
+puts "2. Le handle le plus court est : #{shortest_handle}"
+#Ici j'utilise la fonction .minby afin de trouver le "handle" le plus court de l'array
+#Le calcul est centré sur "handle" pour qu'il me donne le résultat via la fonction length qui opère dans le "tableau"
+
+
+# QUESTION 3: Combien y-a-t-il de handle contenant 5 caractères ?
+handles_with_5_characters = handles.count { |handle| handle.length == 6 }
+puts "3. Il y a #{handles_with_5_characters} handles contenant 5 caractères."
+ #Ici dans le calcul j'ai utilisé la fonction strictement égal "==" afin de compter 6 caractères, car '@' est inclus
+
+
+# QUESTION 4: Combien commencent par une majuscule ?
+handles_starting_with_capital = handles.count { |handle| handle[1] =~ /[A-Z]/ }
+puts "4. Il y a #{handles_starting_with_capital} handles commençant par une majuscule."
+#Ici le calcul va se concentrer sur le deuxième élément du tableau avec l'index [1] qui est donc une lettre et pas un "@"
+#L'opérateur =~ est utilisé en Ruby pour tester si une expression régulière correspond à une chaîne de caractères. 
+#Il renvoie l'indice de la première correspondance s'il y en a une, sinon il renvoie nil.
+#La méthode /[A-Z]/ dans le calcul Vérifie si la chaîne contient au moins un caractère majuscule,c'est un genre de scan
+
+# QUESTION 5: Trie la liste de handle par ordre alphabétique.
+sorted_handles_alphabetically = handles.sort
+puts "5. Liste des handles triée par ordre alphabétique :"
+puts sorted_handles_alphabetically
+#Ici je me suis simplement servi de la fonction .sort 
+#Cette fonction va trier les éléments d'une collection, telle qu'un tableau, en ordre croissant par défaut.
+
+
+
+# QUESTION 6: Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+sorted_handles_by_length = handles.sort_by { |handle| handle.length }
+puts "6. Liste des handles triée par taille :"
+puts sorted_handles_by_length
+#Ici je réutilise ma fonction .sort avec sort_by pour l'appliquer a mon calcul qui va sapliquer sur ma liste
+
+# QUESTION 7: Quelle est la position dans l'array de la personne @epenser ?
+position_of_epenser = handles.index("@epenser")
+puts "7. La position de @epenser dans l'array est : #{position_of_epenser}"
+#Ici j'ai utilisé la fonction .index tout simplement pour trouver la position du mail désiré
+
+# QUESTION 8: Sors-moi une répartition des handle par taille de ces derniers
+handles_by_length_distribution = handles.group_by { |handle| handle.length }
+puts "8. Répartition des handles par taille :"
+handles_by_length_distribution.each do |length, handles|
+  puts "#{length} caractère(s): #{handles.length} handle(s)"
+end
+#Utilisation de la fonction group_by que j'applique a mon tableau avec le calcul ci dessus
+#Utilisation de .each do qui est précédé de group_by
+
